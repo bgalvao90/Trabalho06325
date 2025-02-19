a14a30860e294558f2ef01752d079d8ad4df13ad
048783 <048783@UNI-LAB2-35>
2025-02-18 21:24:15 -0300

    Adicionar arquivos de projeto.

---
+++ Trabalho06325/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class URI
+{
+
+    static void Main(string[] args)
+    {
+
+        int ValorEntrada = int.Parse(Console.ReadLine());
+
+        int impar = 1;
+
+        do
+        {
+            Console.WriteLine(impar);
+            impar = impar + 2;
+
+        }
+        while (impar <= ValorEntrada);
+        Console.Read();
+
+    }
+
+}
