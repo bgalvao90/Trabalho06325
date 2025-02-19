57cb670f83fa7eef3416080c76778a10cf525a70
048783 <048783@UNI-LAB2-35>
2025-02-19 19:46:03 -0300

    exercicio 1071

---
+++ 1071/Program.cs
@@ -0,0 +1,23 @@
+﻿int x = int.Parse(Console.ReadLine());
+int y = int.Parse(Console.ReadLine());
+
+
+if (x > y)
+{
+    int diferenca = x;
+    x = y;
+    y = diferenca;
+}
+
+int soma = 0;
+
+
+for (int i = x + 1; i < y; i++)
+{
+    if (i % 2 != 0)  
+    {
+        soma += i;
+    }
+}
+
+Console.WriteLine(soma);
