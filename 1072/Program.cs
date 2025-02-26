04e692d8cb9bfc7eda952c518a2bf85dd19176e5
048783 <048783@UNI-LAB2-35>
2025-02-26 20:00:34 -0300

    1072

--- 1072/Program.cs
+++ 1072/Program.cs
@@ -1,12 +1,26 @@
-﻿int n = int.Parse(Console.ReadLine());  
-
+﻿int n = int.Parse(Console.ReadLine());
+int b = 0;
+int y = 0;
 for  (int i = 0; i < n; i++)
 {
-   
-    int x = int.Parse(Console.ReadLine());
-    if ((x < Math.Pow(-10, 7)) && (x > Math.Pow(10, 7)))
-    {
-
-    } 
     
+    int x = int.Parse(Console.ReadLine());
+    if ((x > Math.Pow(-10,7)) && (x < Math.Pow(10,7)))
+    {
+        if ((x >= 10) && (x <= 20))
+        {
+            
+            b = b+ 1;
+            
+        }
+        else
+        {
+            
+            y = y+ 1;
+            
+        }
+   } 
+
 }
+Console.WriteLine($"{b} IN");
+Console.WriteLine($"{y} OUT");
