46b20c68bc26d47cbad9dad303e93d0c7aa392f0
usuario <usuario@DESKTOP-RSA23LP>
2025-02-27 23:07:01 -0300

    1074

---
+++ 1074/Program.cs
@@ -0,0 +1,22 @@
+﻿int n = int.Parse(Console.ReadLine());
+int [] valor = new int[n];
+
+
+
+for (int i = 0; i < n; i++)
+{
+
+    valor[i] = int.Parse(Console.ReadLine());
+        if (valor[i] % 2 == 0 && valor[i] > 0)  
+              Console.WriteLine("EVEN POSITIVE");
+    else if (valor[i] % 2 == 0 && valor[i] < 0)
+        Console.WriteLine("EVEN NEGATIVE");
+    else if (valor[i] % 2 != 0 && valor[i] > 0)
+        Console.WriteLine("ODD POSITIVE");
+    else if (valor[i] % 2 != 0 && valor[i] < 0)
+        Console.WriteLine("ODD NEGATIVE");
+    
+    else if (valor[i] == 0)
+        Console.WriteLine("NULL");
+    
+}
