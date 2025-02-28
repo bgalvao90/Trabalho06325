4b147e2a5bc5f8bd18b5a9626151508392ef480f
usuario <usuario@DESKTOP-RSA23LP>
2025-02-28 00:54:50 -0300

    1096

---
+++ 1096/Program.cs
@@ -0,0 +1,11 @@
+﻿ 
+
+for (int i = 1; i <= 9; i++)
+{
+    
+    int j = 7;
+    Console.WriteLine($"I={i} J={j}");
+    Console.WriteLine($"I={i} J={j - 1}");
+    Console.WriteLine($"I={i} J={j - 2}");
+    i = i + 1;
+}
