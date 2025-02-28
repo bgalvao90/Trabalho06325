986f0675785ab6e492d9f2944e796b9622600486
usuario <usuario@DESKTOP-RSA23LP>
2025-02-28 00:13:41 -0300

    1080

---
+++ 1080/Program.cs
@@ -0,0 +1,17 @@
+﻿int n = 100;
+    int [] valores = new int[n];
+int maior = 0;
+int posicao = 0;
+for (int i = 0; i < n; i++)
+{
+    
+    valores[i] = int.Parse(Console.ReadLine());
+    if (valores[i] > maior)
+    {
+        maior = valores[i];
+        posicao = i + 1;
+    }
+
+}
+Console.WriteLine(maior);
+Console.WriteLine(posicao);
