07b7225f55b7d82038b5b022fb2da404e4b1e004
usuario <usuario@DESKTOP-RSA23LP>
2025-03-01 01:50:37 -0300

    1151

---
+++ 1151/Program.cs
@@ -0,0 +1,28 @@
+﻿int n = int.Parse(Console.ReadLine());
+if (n <= 0 || n >= 46)
+    return;
+
+int num, num1;
+num = 0;
+num1 = 1;
+
+
+for (int i = 0; i < n; i++)
+{
+   if (i == n - 1)
+    {
+        
+        Console.WriteLine($"{num}");
+
+
+    }
+    else
+    {
+        Console.WriteLine($"{num} ");
+        int temp = num + num1;
+        num = num1;
+        num1 = temp;
+    }
+
+
+}
