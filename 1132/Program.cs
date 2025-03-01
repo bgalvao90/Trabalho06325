f29029a7c21920b440fcecfe3a537db8c657363d
usuario <usuario@DESKTOP-RSA23LP>
2025-03-01 00:41:09 -0300

    1132

---
+++ 1132/Program.cs
@@ -0,0 +1,16 @@
+﻿int x = int.Parse(Console.ReadLine());
+int y = int.Parse(Console.ReadLine());
+
+int inicio =Math.Min(x, y);
+int fim = Math.Max(x, y);
+
+int soma = 0;
+
+for (int i = inicio; i <= fim; i++)
+{
+    if (i % 13 != 0)
+    {
+        soma += i;
+    }
+}
+Console.WriteLine(soma);
