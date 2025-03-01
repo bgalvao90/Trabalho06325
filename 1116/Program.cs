37a977d3551354d8b2ee6aeebb19232207377a37
usuario <usuario@DESKTOP-RSA23LP>
2025-03-01 00:34:33 -0300

    1116

---
+++ 1116/Program.cs
@@ -0,0 +1,19 @@
+﻿int n = int.Parse(Console.ReadLine());
+
+int[] vetor = new int [n];
+
+for (int i = 0; i < n; i++)
+{
+    string[] valores = Console.ReadLine().Split();
+    double x = double.Parse(valores[0]);
+    double y = double.Parse(valores[1]);
+    if (y == 0)
+    {
+        Console.WriteLine("divisao impossivel");
+    }
+    else
+    {
+        Console.WriteLine($"{(x / y):F1}");
+    }
+
+}   
