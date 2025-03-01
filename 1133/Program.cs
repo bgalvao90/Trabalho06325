82a6f9e59c1ce56fb957fa9eb0ad01e1deb088a3
usuario <usuario@DESKTOP-RSA23LP>
2025-03-01 00:57:26 -0300

    1133

---
+++ 1133/Program.cs
@@ -0,0 +1,13 @@
+﻿int x = int.Parse(Console.ReadLine());
+int y = int.Parse(Console.ReadLine());
+
+int inicio = Math.Min(x, y);
+int fim = Math.Max(x, y);
+
+for (int i = inicio; i < fim; i++)
+{
+    if (i % 5 == 2 || i % 5 == 3)
+    {
+        Console.WriteLine(i);
+    }
+}
---
+++ 1134/Program.cs
@@ -0,0 +1,37 @@
+﻿
+int totalAlcool = 0;  
+int totalGasolina = 0;  
+int totalDiesel = 0;  
+for (int i = 0; i < 10000; i++)
+{
+    int combustivel = int.Parse(Console.ReadLine());
+    if(combustivel < 1 && combustivel > 4)
+    {
+        combustivel = int.Parse(Console.ReadLine());
+
+    }
+    else {
+        {
+            if (combustivel == 1)
+            {
+                totalAlcool += 1;
+            }
+            else if (combustivel == 2)
+            {
+                totalGasolina += 1;
+            }
+            else if (combustivel == 3)
+            {
+                totalDiesel += 1;
+            }
+            else if (combustivel == 4)
+            {
+                break;
+            }
+        } }
+   
+}
+Console.WriteLine("MUITO OBRIGADO");
+Console.WriteLine($"Alcool: {totalAlcool}");
+Console.WriteLine($"Gasolina: {totalGasolina}");
+Console.WriteLine($"Diesel: {totalDiesel}");
